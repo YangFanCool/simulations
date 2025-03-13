c73a54fe4f2dd2589b1bd2b6de9f4a614149c0e5
xxx <xxx@xxx.com>
2025-03-13 21:13:35 +0800

    udpate for 128 run

--- nyx/Exec/LyA/run.sh
+++ nyx/Exec/LyA/run.sh
@@ -8,6 +8,16 @@ if [ $# -eq 1 ] && [ "$1" -eq 0 ]; then
     exit 0
 fi
 
+if [ $# -eq 1 ] && [ "$1" -eq 1 ]; then
+    rm -rf /root/autodl-tmp/fan/nyx_128_per_100 2>/dev/null
+    mkdir /root/autodl-tmp/fan/nyx_128_per_100
+fi
+
+if [ $# -eq 1 ] && [ "$1" -eq 2 ]; then
+    rm -rf /root/autodl-tmp/fan/nyx_128_per_10 2>/dev/null
+    mkdir /root/autodl-tmp/fan/nyx_128_per_10
+fi
+
 config=$1
 cuda_device=$((config - 1))
 export CUDA_VISIBLE_DEVICES=$cuda_device
